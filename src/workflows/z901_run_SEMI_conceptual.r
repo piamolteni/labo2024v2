724d8d3cbe9b3eb64b1806fde7d10129de5206b0
piamolteni <molteni.mpia@gmail.com>
2024-11-24 13:26:52 +0000

    auto commit 28       20241124 132645

--- src/workflows/z901_run_SEMI_conceptual.r
+++ src/workflows/z901_run_SEMI_conceptual.r
@@ -1,7 +1,7 @@
 require("rlang")
 
 # workflow que voy a correr
-PARAM <- "src/workflows/991_workflow_SEMI_conceptual_2.r"
+PARAM <- "src/workflows/991_workflow_SEMI_conceptual.r"
 
 envg <- env()
 
